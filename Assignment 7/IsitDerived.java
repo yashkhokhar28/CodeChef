e8298fa3595afdb13a7a12dd875367911f7ea3b9
Yash Khokhar <yashkhokhar28@gmail.com>
2022-10-30 12:26:28 +0530

    Update IsitDerived.java

--- Assignment 7/IsitDerived.java
+++ Assignment 7/IsitDerived.java
@@ -1,7 +1,6 @@
 /* package codechef; // don't place package name! */
 
-import java.lang.*;
-import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /* Name of the class has to be "Main" only if the class is public. */
@@ -9,20 +8,28 @@ public class IsitDerived {
     public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
         int testcase = sc.nextInt();
-        while (testcase-->0){
-            int [] array1 = new int[100];
+        while (testcase-- > 0) {
             int size = sc.nextInt();
+            int[] array1 = new int[size];
             for (int i = 0; i < size; i++) {
                 array1[i] = sc.nextInt();
             }
-            for (int j = size-1; j >=0; j--) {
-                System.out.println(array1[j]);
+            int[] array2 = new int[size];
+            int[] array3 = new int[size];
+            for (int j = 0; j < (size) / 2; j++) {
+                array2[j] = array1[j];
+//                System.out.print(array2[j] + " ");
             }
-//            if (flag){
-//                System.out.println("Yes");
-//            } else {
-//                System.out.println("No");
-//            }
+            System.out.println(" ");
+            for (int j = (array1.length / 2); j < size; j++) {
+                array3[j] = array1[j];
+//                System.out.print(array3[j] + " ");
+            }
+            if (Arrays.equals(array2, array3)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
         }
     }
 }
