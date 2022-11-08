d9a25c8a83c6b4a0ad165898fcf0a731492a6ec0
Yash Khokhar <yashkhokhar28@gmail.com>
2022-11-08 13:43:54 +0530

    Update CorrectSlippers.java

--- Assignment 1/CorrectSlippers.java
+++ Assignment 1/CorrectSlippers.java
@@ -5,8 +5,8 @@ import java.lang.*;
 import java.io.*;
 
 /*Defenation---
-Chef has NN slippers, LL of which are left slippers and the rest are right slippers. Slippers must always be sold in pairs, where each pair contains one left and one right slipper. If each pair of slippers cost XX rupees, what is the maximum amount of rupees that Chef can get for these slippers?*/
-/* Name of the class has to be "Main" only if the class is public. */
+Chef has NN slippers, LL of which are left slippers and the rest are right slippers. Slippers must always be sold in pairs, where each pair contains one left and one right slipper. If each pair of slippers cost XX rupees, what is the maximum amount of rupees that Chef can get for these slippers?
+Name of the class has to be "Main" only if the class is public. */
 class Codechef {
     public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
